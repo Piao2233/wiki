6b97ec3ce031f0cd12876b215da1066fc3be5fcb
piao <2119828525@qq.com>
2021-08-29 17:19:12 +0800

    init

---
+++ src/test/java/com/czy/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czy.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
